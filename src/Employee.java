4435fb2ed967e3a43e016c36ea3d61ac103c8fb5
Kateryna Nuzhna <katranchik21@gmail.com>
2024-10-25 18:16:17 +0200

    finaly commit1

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 /*
 Создать класс Employee c полями id, name, age, salary.
  */
@@ -5,9 +7,9 @@ public class Employee implements Comparable<Employee>{
     private int id;
     private String name;
     private int age;
-    private int salary;
+    private BigDecimal salary;
 
-    public Employee(int id, String name, int age, int salary) {
+    public Employee(int id, String name, int age, BigDecimal salary) {
         this.id = id;
         this.name = name;
         this.age = age;
@@ -26,20 +28,29 @@ public class Employee implements Comparable<Employee>{
         return age;
     }
 
-    public int getSalary() {
+    public BigDecimal getSalary() {
         return salary;
     }
 
     @Override
     public String toString() {
-        return String.format("id работника: %d, Имя: %s, %d года, Зарплата: %d "
-                , id,name,age,salary)+System.lineSeparator();
-
+        return "Employee " +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary;
     }
-
 
     @Override
     public int compareTo(Employee o) {
-        return salary-o.getSalary();
+        return salary.compareTo(o.getSalary());
     }
+
+
+
+/*mij metod
+    @Override
+    public int compareTo(Employee o) {
+        return salary-o.getSalary();
+    }*/
 }
--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,38 @@
 3. Создайте массив объектов Employee и отсортируйте его по зарплате
  */
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
      Employee[] arrayEmployee ={
-       new Employee(1,"Петров В.",25,1750),
-       new Employee(2,"Лупкин Ж.",34,2250),
-       new Employee(3,"Смирнова А.",20,1550),
-       new Employee(4,"Карпачов С.",45,3550),
-       new Employee(5,"Саркисян Л.",30,1200),
-       new Employee(6,"Белов О.",38,4000),
-       new Employee(7,"Никитюк Л.",29,2350),
-       new Employee(8,"Кобейн К .",34,1400)
+       new Employee(1,"Петров В.",25,new BigDecimal("1750.60")),
+       new Employee(2,"Лупкин Ж.",34, new BigDecimal("2250.40")),
+       new Employee(3,"Смирнова А.",20,new BigDecimal("1550.50")),
+       new Employee(4,"Карпачов С.",45,new BigDecimal("3350.10")),
+       new Employee(5,"Саркисян Л.",30,new BigDecimal("1200.30")),
+       new Employee(6,"Белов О.",38,new BigDecimal("4000.65")),
+       new Employee(7,"Никитюк Л.",29,new BigDecimal("2350.10")),
+       new Employee(8,"Кобейн К .",34, new BigDecimal("1400.50"))
      };
-        System.out.println(Arrays.toString(arrayEmployee));
+      print(arrayEmployee);
+        Arrays.sort(arrayEmployee);
+        System.out.println("------------сортировка по зарплате------------------");
+        print(arrayEmployee);
+
+        /*System.out.println(Arrays.toString(arrayEmployee));
         Arrays.sort(arrayEmployee);
         System.out.println("------------сортировка по зарплате------------------");
         System.out.println(Arrays.toString(arrayEmployee));
+*/
+        //mij kod
 
-
+      }
+    public static void print(Employee[] employees){
+        for (Employee employee:employees){
+            System.out.println(employee);
+        }
     }
+
 }
